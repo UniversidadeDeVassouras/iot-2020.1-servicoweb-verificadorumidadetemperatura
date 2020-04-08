b01d9bbe9bf7acb33d59c435ea8c2c911f3391c7
Tássio Auad <auadtassio@gmail.com>
2020-04-08 00:52:15 -0300

    feat: Entidades
    Criadas as entidades de temperatura e umidade

---
+++ application/model/entity/umidade.py
@@ -0,0 +1,18 @@
+class Umidade: 
+    def __int__(self, valor): 
+        self._valor = valor
+
+    def get_id(self):
+        return self._id
+    
+    def get_valor(self):
+        return self._valor
+
+    def get_data(self):
+        return self._data
+
+    def set_id(self, id):
+        self._id = id
+
+    def set_data(self, data):
+        self._data = data
